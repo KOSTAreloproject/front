8731151fbda1b36fced68c88a1a3b74276fdce84
KyunglaeKim <klkim3272@gmail.com>
2023-03-06 15:44:11 +0900

    230306 커밋

--- js/zzimlist.js
+++ js/zzimlist.js
@@ -1,50 +1,47 @@
 //--사용자의 찜목록 출력 START//
 $(() => { 
-    let url = backUrl + "zzim/list.do";
-    let $origin = $("div.wishlist").first();
-    let id = 'bbb'
-    let condition = "id="+id;
-    $("div.wishlist").not(":first-child").remove();
+    zzimlist(1)
+    function zzimlist (cp) {
+    let url = backUrl + 'zzim/'+cp
+    let $origin = $("div.mywishlist").first();
+    $("div.mywishlist").not(":first-child").remove();
     $origin.show();
     $.ajax({
+         xhrFields: { 
+                withCredentials: true // 클라이언트와 서버가 통신할때 쿠키와 같은 인증 정보 값을 공유하겠다는 설정
+            },
         url: url,
-        data: condition,
         method: "get",
         success: function (jsonObj) {
-            console.log(jsonObj);
-            let $origin = $("div.wishlist").first();
-            let $parent = $("div.wish");
-            $(jsonObj).each((index, p) => {
-                let pNum = p.pNum;
-                let sFile = p.sFile
-                let sBrand = p.sBrand;
-                let sName = p.sName;
-                let sColor = p.sColor;
-                let sizeCategoryName = p.sizeCategoryName;
-                let sHopePrice = p.sHopePrice;
-                let maxPrice = p.aPrice;
+            
+            let $parent = $("div.mypwish")
+            let sNum = ""
+            $(jsonObj.list).each((index, w) => {
+                let pNum = w.pnum
+                let sBrand = w.sbrand
+                let sName = w.sname
+                let sizeCategoryName = w.sizeCategoryName;
+                let sHopePrice = w.shopePrice;
+                let maxPrice = w.maxPrice;
+                sNum = w.snum
                 if (maxPrice != 0) {
                     sHopePrice = maxPrice;
                 }
-                let sGrade = p.sGrade;
-          let enddate = p.pEndDate;
+          let enddate = w.pendDate;
           let $copy = $origin.clone();
-          // let imgStr = '<img src="../images/' + prodNo + '.jpg">'
-          // $copy.find('div.img').html(imgStr)
-          let $imgObj = $("<img>");
-          $imgObj.attr("src", "../imgs/" + sFile + ".jpg");
-          $imgObj.attr("width", "150px");
-          $imgObj.attr("height", "150px");
-          $copy.find("div.simg").empty().append($imgObj);
-          $copy.find("div.pnum").html(pNum);
-          $copy.find("div.sbrand").html(sBrand);
-          $copy.find("div.sname").html(sName);
-          $copy.find("div.scolor").html("색상 : " + sColor);
-          $copy.find("div.sizecategoryname").html("사이즈 : " + sizeCategoryName);
-          $copy.find("div.sgrade").html("등급 : " + sGrade);
-          $copy.find("div.maxprice").html("최고입찰가 : " + sHopePrice + "원");
-          $copy.find("div.enddate").html("경매 마감일 : " + enddate);
-          $parent.append($copy);
+          let $imgObj = $("<img>")
+          $imgObj.attr("id",sNum)
+          $imgObj.attr("width", "200px")
+          $imgObj.attr("height", "200px")
+          $copy.find("div.wzimg").empty().append($imgObj)
+          $copy.find("div.wpnum").html(pNum)
+          $copy.find("div.wsbrand").html(sBrand)
+          $copy.find("div.wsname").html(sName)
+          $copy.find("div.wsizecategoryname").append(sizeCategoryName)
+          $copy.find("div.wenddate").append(enddate)
+
+          $parent.append($copy)
+          zimgShow(sNum)
         });
         $origin.hide();
     },
@@ -52,32 +49,56 @@ $(() => {
         alert(xhr.status);
     },
 });
+    }
 //--사용자의 찜목록 출력 END//
+//--찜 목록 이미지 출력 START//
+ function zimgShow(num){
+    $.ajax({
+      xhrFields: {
+        responseType: 'blob',
+        withCredentials: true,
+        cache: false, 
+      },
+      url: backUrl+'product/list/img/'+num,
+      method: "get",
+      success: function (result) {
+      console.log(result)
+      let blobStr = URL.createObjectURL(result)
+      $('img#'+num).attr('src', blobStr)
+    },
+    error: function (xhr) {
+      console.log(xhr.status)
+    },
+  })
+}
+//--찜 목록 이미지 출력 END//
 //찜하기 삭제 START//
  $("div.wish").on("click", "button.zsubmit", (e) =>{
-     let pNum = $(e.target).parents("div.wishlist").find("div.pnum").html();
-    let id = "bbb";
-    let url = backUrl + "zzim/del.do";
-    let data ={"pNum":pNum, "id":id}
-        $.ajax({
-            url: url,
-            data: data,
-            method: "post",
-            success: function (jsonObj){
-                alert(jsonObj)
-                location.href = "./mypage_zzimlist.html"
-            },
-            error: function (xhr) {
-                alert(xhr.status);
-            },
-        });
+    let pNum = $(e.target).parents("div.wishlist").find("div.pnum").html();
+    let url = backUrl + "zzim/"+pNum;
+       $.ajax({
+      xhrFields: {
+        withCredentials: true, // 클라이언트와 서버가 통신할때 쿠키와 같은 인증 정보 값을 공유하겠다는 설정
+      },
+      url: url,
+      method: "delete",
+      success: function (result) {
+       let msg = result.msg
+        alert(msg)
+       location.href = frontUrl + 'mypage_zzimlist.html'
+       },
+       error: function (xhr) {
+        alert(xhr.status)
+      } 
     });
+})
+    
     //찜하기 삭제 END//
     //상품사진 클릭시 상세화면 이동 START//
-    $("div.wish").on("click", "div.simg", (e) => {
-        let pNum = $(e.target).parents("div.wishlist").find("div.pnum").html();
-        location.href = "./productdetail.html?pNum=" + pNum;
-    });
+    $("div.mypwish").on("click", "div.wzimg>img", (e) => {
+        let pNum = $(e.target).parents("div.mywishlist").find("div.wpnum").html();
+        location.href = frontUrl +'shopproductdetail.html?pNum=' + pNum
+    })
     //상품사진 클릭시 상세화면 이동 END//
 
     //입찰하러 가기 버튼클릭시 할일 START//
@@ -89,7 +110,5 @@ $(() => {
   });
 
   
-  
 });
-    
 
